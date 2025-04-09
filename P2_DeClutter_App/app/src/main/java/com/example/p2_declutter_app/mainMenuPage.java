5bd6560aeb2df16b2bc38a9158b1804fa114cb79
AagmaPekol <aagaard100@gmail.com>
2025-04-09 15:22:08 +0200

    Added basic activites(pages)

---
+++ P2_DeClutter_App/app/src/main/java/com/example/p2_declutter_app/mainMenuPage.java
@@ -0,0 +1,78 @@
+package com.example.p2_declutter_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class mainMenuPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main_menu_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button deClutterBtn = findViewById(R.id.deClutterBtn);
+        deClutterBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mainMenuPage.this, deClutterOverviewPage.class);
+                startActivity(intent);
+            }
+        });
+
+        Button wardrobeBtn = findViewById(R.id.wardrobeBtn);
+        wardrobeBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(mainMenuPage.this, wardrobePage.class);
+                startActivity(intent);
+            }
+        });
+
+        Button rewardsBtn = findViewById(R.id.rewardsBtn);
+        rewardsBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                notImplemented();
+            }
+        });
+
+        Button challengesBtn = findViewById(R.id.challengesBtn);
+        challengesBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                notImplemented();
+            }
+        });
+
+        Button loginBtn = findViewById(R.id.profileBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mainMenuPage.this,loginPage.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+
+    public void notImplemented(){
+        Toast.makeText(this, "This is not implemented yet", Toast.LENGTH_SHORT).show();
+    }
+}
