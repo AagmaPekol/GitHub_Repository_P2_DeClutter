6c1c4ac9511e6b1c26691920135e65a8bf1f02b1
AagmaPekol <116688650+AagmaPekol@users.noreply.github.com>
2025-04-22 23:29:34 +0200

    renamed some stuff

--- P2_DeClutter_App/app/src/main/java/com/example/p2_declutter_app/database/Clothing.java
+++ P2_DeClutter_App/app/src/main/java/com/example/p2_declutter_app/database/Clothing.java
@@ -1,21 +1,22 @@
-package com.example.p2_declutter_app;
+package com.example.p2_declutter_app.database;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "DB_clothing_items")
-public class DBClothingItem {
+public class Clothing {
 
     @PrimaryKey(autoGenerate = true)
     public int id;
 
-    @ColumnInfo(name = "clothing_Type")
+    @ColumnInfo(name = "clothing_type")
     public String clothingType;
 
+    @ColumnInfo(name = "clothing_description")
     public String description;
 
-    public DBClothingItem(String clothingType, String description){
+    public Clothing(String clothingType, String description){
         this.clothingType = clothingType;
         this.description = description;
     }
