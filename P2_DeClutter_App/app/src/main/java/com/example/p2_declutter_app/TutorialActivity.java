e303df777ab9b5998354db19c6b0405d1cce857c
SoldierTheLost <jonas.holst@live.dk>
2025-04-24 17:53:52 +0200

    tutorial v0.4
    tutorial er blevet sat op sådan man skal, når vi finder ud af hvad side den skal tilhører så kan man bare ændre navnet hvor den skal være så burde det virke - J

---
+++ P2_DeClutter_App/app/src/main/java/com/example/p2_declutter_app/TutorialActivity.java
@@ -0,0 +1,54 @@
+package com.example.p2_declutter_app;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.firebase.inappmessaging.model.Button;
+
+public class TutorialActivity extends AppCompatActivity {
+    private ViewPager2 viewPager;
+    private Button skipButton, nextButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        if (!prefs.getBoolean("first_time", true)) {
+            startMainApp();
+            return;
+        }
+
+        setContentView(R.layout.activity_tut);
+        viewPager = findViewById(R.id.viewPager);
+        skipButton = findViewById(R.id.skip_button);
+        nextButton = findViewById(R.id.next_button);
+
+        TutorialAdapter adapter = new TutorialAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        skipButton.setOnClickListener(v -> startMainApp());
+
+        nextButton.setOnClickListener(v -> {
+            if (viewPager.getCurrentItem() < adapter.getItemCount() - 1) {
+                viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+            } else {
+                startMainApp();
+            }
+        });
+    }
+
+    private void startMainApp() {
+        SharedPreferences.Editor editor = getSharedPreferences("prefs", MODE_PRIVATE).edit();
+        editor.putBoolean("first_time", false);
+        editor.apply();
+
+        Intent intent = new Intent(this, MainActivity.class); //Her skal den hedder hvor tutorualen skal være - J
+        startActivity(intent);
+        finish();
+    }
+}
