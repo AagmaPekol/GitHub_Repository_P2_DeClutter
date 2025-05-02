1bfad68cf805c6404aab751d016e9087328cdcd6
MJulia01 <malgosia.u@interia.eu>
2025-05-02 13:33:28 +0200

    pickclothingtype but better

--- P2_DeClutter_App/app/src/main/java/com/example/p2_declutter_app/MusicBackground.java
+++ P2_DeClutter_App/app/src/main/java/com/example/p2_declutter_app/MusicBackground.java
@@ -7,35 +7,35 @@ import android.media.MediaPlayer;
 import android.os.IBinder;
 
 public class MusicBackground {
-    private MediaPlayer mediaPlayer;
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        mediaPlayer = MediaPlayer.create(this, R.raw.background_music); //Here the music file should be
-            mediaPlayer.start();
-        mediaPlayer.setLooping(true); // Loop the music ere
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        if (!mediaPlayer.isPlaying()) {
-            mediaPlayer.start();
-        }
-        return START_STICKY; // Keeps service running
-    }
-
-    @Override
-    public void onDestroy() {
-        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
-            mediaPlayer.stop();
-            mediaPlayer.release();
-        }
-        super.onDestroy();
-    }
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null; // We don’t need to bind
-    }
+//    private MediaPlayer mediaPlayer;
+//
+//    @Override
+//    public void onCreate() {
+//        super.onCreate();
+//        mediaPlayer = MediaPlayer.create(this, R.raw.background_music); //Here the music file should be
+//            mediaPlayer.start();
+//        mediaPlayer.setLooping(true); // Loop the music ere
+//    }
+//
+//    @Override
+//    public int onStartCommand(Intent intent, int flags, int startId) {
+//        if (!mediaPlayer.isPlaying()) {
+//            mediaPlayer.start();
+//        }
+//        return START_STICKY; // Keeps service running
+//    }
+//
+//    @Override
+//    public void onDestroy() {
+//        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+//            mediaPlayer.stop();
+//            mediaPlayer.release();
+//        }
+//        super.onDestroy();
+//    }
+//
+//    @Override
+//    public IBinder onBind(Intent intent) {
+//        return null; // We don’t need to bind
+//    }
 }
