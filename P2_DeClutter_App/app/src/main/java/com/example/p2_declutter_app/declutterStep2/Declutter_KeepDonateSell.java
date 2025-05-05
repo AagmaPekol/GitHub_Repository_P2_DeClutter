5181ed1dd5a31c7cf8f7a35ddd0039b0960ededf
AagmaPekol <116688650+AagmaPekol@users.noreply.github.com>
2025-05-05 05:43:04 +0200

    Made swipe function work

--- P2_DeClutter_App/app/src/main/java/com/example/p2_declutter_app/declutterStep2/Declutter_KeepDonateSell.java
+++ P2_DeClutter_App/app/src/main/java/com/example/p2_declutter_app/declutterStep2/Declutter_KeepDonateSell.java
@@ -1,9 +1,12 @@
 package com.example.p2_declutter_app.declutterStep2;
 
+import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,17 +23,17 @@ import com.bumptech.glide.request.RequestOptions;
 
 import java.io.File;
 
-public class Declutter_KeepDonateSell extends AppCompatActivity implements GestureDetector.OnGestureListener {
+public class Declutter_KeepDonateSell extends AppCompatActivity {
 
     private Bundle bundle;
     private TextView descriptionText;
     private ImageView photoView;
+    private  FrameLayout frameLayout;
 
-    private static final String TAG = "Swipe Position";
-    private float x1, x2, y1, y2;
-    private static int MIN_DISTANCE = 150;
-    private GestureDetector gestureDetector;
+    private String decision = "";
 
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,13 +42,79 @@ public class Declutter_KeepDonateSell extends AppCompatActivity implements Gestu
 
         bundle = getIntent().getExtras();
 
-        //descriptionText = findViewById(R.id.aiDescriptionText);
-        //photoView = findViewById(R.id.photoView2);
+        descriptionText = findViewById(R.id.aiDescriptionText);
+        photoView = findViewById(R.id.photoView2);
+        frameLayout = findViewById(R.id.frameLayout);
 
         descriptionText.setText(bundle.getString("text_AI"));
         setImage();
 
-        this.gestureDetector = new GestureDetector(Declutter_KeepDonateSell.this, this);
+        // Got code from this tutorial:
+        // https://www.youtube.com/watch?v=Rd89cVKrQBg
+        frameLayout.setOnTouchListener(new View.OnTouchListener() {
+            float startX = 0f;
+            float startY = 0f;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        // Store the initial touch position
+                        startX = event.getX();
+                        startY = event.getY();
+                        return true;
+
+                    case MotionEvent.ACTION_MOVE:
+                        // Puts the frameLayout where the touch is
+                        float moveX = event.getRawX() - frameLayout.getWidth() / 2;
+                        float moveY = event.getRawY() - frameLayout.getHeight() / 2;
+                        frameLayout.setX(moveX);
+                        frameLayout.setY(moveY);
+                        return true;
+
+                    case MotionEvent.ACTION_UP:
+                        // When the frameLayout is no longer touched.
+                        float endX = event.getX();
+                        float endY = event.getY();
+
+                        float deltaX = endX - startX;
+                        float deltaY = endY - startY;
+
+                        final int SWIPE_THRESHOLD = 100;
+
+                        if (Math.abs(deltaX) > Math.abs(deltaY)) {
+                            if (deltaX > SWIPE_THRESHOLD) {
+                                decision = "keep"; // Swiped right
+                                Toast.makeText(Declutter_KeepDonateSell.this, "swiped right", Toast.LENGTH_SHORT).show();
+                            } else if (deltaX < -SWIPE_THRESHOLD) {
+                                decision = "sell"; // Swiped left
+                                Toast.makeText(Declutter_KeepDonateSell.this, "swiped left", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            if (deltaY < -SWIPE_THRESHOLD) {
+                                decision = "donate"; // Swiped up
+                                Toast.makeText(Declutter_KeepDonateSell.this, "swiped up", Toast.LENGTH_SHORT).show();
+                            }
+                            else if (deltaY > SWIPE_THRESHOLD) {
+                                decision = "delete"; // Swiped down
+                                Toast.makeText(Declutter_KeepDonateSell.this, "swiped down", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        // Reset frameLayout position to center
+                        frameLayout.animate()
+                                .x((v.getRootView().getWidth() - frameLayout.getWidth()) / 2f)
+                                .y((v.getRootView().getHeight() - frameLayout.getHeight()) / 2f)
+                                .setDuration(150)
+                                .start();
+
+                        return true;
+                }
+                return false;
+            }
+        });
+
     }
 
     private void setImage() {
@@ -62,72 +131,5 @@ public class Declutter_KeepDonateSell extends AppCompatActivity implements Gestu
                 Toast.makeText(this, "Image file not found", Toast.LENGTH_SHORT).show();
             }
         }
-    }
-
-    //Credit to this youtube video on how to implement swipe gestures
-    //https://www.youtube.com/watch?v=oFl7WwEX2Co
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        gestureDetector.onTouchEvent(event);
-
-        switch(event.getAction()){
-            case MotionEvent.ACTION_DOWN:
-                x1 = event.getX();
-                y1 = event.getY();
-                break;
-            case MotionEvent.ACTION_UP:
-                x2 = event.getX();
-                y2 = event.getY();
-
-                float valueX = x2 - x1;
-                float valueY = y2 - y1;
-
-                if(Math.abs(valueX) > MIN_DISTANCE){
-                    if(x2 > x1){
-                        Toast.makeText(this, "Swipe to the right", Toast.LENGTH_SHORT).show();
-                }
-                    else{
-                        Toast.makeText(this, "Swipe to the left", Toast.LENGTH_SHORT).show();
-                    }
-                }
-                else if(Math.abs(valueY) > MIN_DISTANCE) {
-                    if (y2 > y1) {
-                        Toast.makeText(this, "Swipe down", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(this, "Swipe up", Toast.LENGTH_SHORT).show();
-                    }
-                }
-        }
-        return super.onTouchEvent(event);
-    }
-
-    @Override
-    public boolean onDown(@NonNull MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public void onShowPress(@NonNull MotionEvent e) {
-
-    }
-
-    @Override
-    public boolean onSingleTapUp(@NonNull MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public boolean onScroll(@Nullable MotionEvent e1, @NonNull MotionEvent e2, float distanceX, float distanceY) {
-        return false;
-    }
-
-    @Override
-    public void onLongPress(@NonNull MotionEvent e) {
-
-    }
-
-    @Override
-    public boolean onFling(@Nullable MotionEvent e1, @NonNull MotionEvent e2, float velocityX, float velocityY) {
-        return false;
     }
 }
