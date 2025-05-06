47db2176fbe38c27cb2d2dff01b435794fbcc937
Andersusb <asbs24@student.aau.dk>
2025-05-07 00:36:06 +0200

    keepPage

--- P2_DeClutter_App/app/src/main/java/com/example/p2_declutter_app/declutterStep3/Declutter_Keep.java
+++ P2_DeClutter_App/app/src/main/java/com/example/p2_declutter_app/declutterStep3/Declutter_Keep.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+
 import com.example.p2_declutter_app.R;
 
 public class Declutter_Keep extends AppCompatActivity {
@@ -13,7 +15,7 @@ public class Declutter_Keep extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_declutter_keep);
 
-        Button nextButton = findViewById(R.id.next_button_keep);
+        ImageButton nextButton = findViewById(R.id.next_button_keep);
         nextButton.setOnClickListener(v -> {
             Intent intent = new Intent(Declutter_Keep.this, declutterKeep2.class);
             startActivity(intent);
